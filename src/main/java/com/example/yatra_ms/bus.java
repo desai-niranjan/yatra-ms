640e00f216204b0cbcc3f3cd2d9e35b90a9dc745
Niranjan Desai <desainiranjan8@gmail.com>
2024-07-01 14:43:40 +0530

    Adding new class bus

---
+++ src/main/java/com/example/yatra_ms/bus.java
@@ -0,0 +1,12 @@
+package com.example.yatra_ms;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class bus {
+    @GetMapping("/bus")
+    public String getData() {
+        return  "Please book your bus ticket from holiday app on 20% discount" ;
+    }
+
+}
